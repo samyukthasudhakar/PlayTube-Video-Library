863bfa91e89eedd18f1866dd207ff39785a7ccf7
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-03-28 20:50:04 +0530

    assets : images and icons added

---
+++ src/assets/icons/index.js
@@ -0,0 +1,17 @@
+import history from './history.png'
+import home from './home.png'
+import like from './like.png'
+import playlist from './playlist.png'
+import watchLater from './watchLater.png'
+import youtube from './youtube.png'
+import menu from './menu.png'
+
+export {
+    history,
+    home,
+    like,
+    playlist,
+    watchLater,
+    youtube,
+    menu
+}
