e0d65ce4f9740815885bb8e8ff78e17798fb5b08
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-03-30 21:55:28 +0530

    refactor : removed unnecessary div's in login and singup pages

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -10,34 +10,30 @@ function Login(){
     const { loginHandler } = useAuth()
 
     return(
-        <div>
-            <div className="flex-center auth-parent">    
-                <div>
-                <form className="flex-column glass-card login-form space-between" onSubmit={(e)=>loginHandler(e, user)}>
-                    <div className="flex-column flex-center">
-                        <Link to="/" className="flex-center">
-                            <img className="header-logo" src={logo}
-                                alt="playtube logo"/>
-                        <span className="header-title">PlayTube</span>
-                        </Link>
-                        <input type="text" placeholder="Email" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, email:e.target.value})} />
-                        <input type="password" placeholder="Password" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, password:e.target.value})} />
-                        <div className="flex-layout space-between w-100">
-                            <div className="flex-layout">
-                                <input id='remember-me' type="checkbox"/> 
-                                <label for='remember-me' className="fs-13px">Remember me</label>
-                            </div>
-                            <Link className="link-txt" to="*">Forgot Password ?</Link>      
+        <div className="flex-center auth-parent">    
+            <form className="flex-column glass-card login-form space-between" onSubmit={(e)=>loginHandler(e, user)}>
+                <div className="flex-column flex-center">
+                    <Link to="/" className="flex-center">
+                        <img className="header-logo" src={logo}
+                            alt="playtube logo"/>
+                    <span className="header-title">PlayTube</span>
+                    </Link>
+                    <input type="text" placeholder="Email" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, email:e.target.value})} />
+                    <input type="password" placeholder="Password" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, password:e.target.value})} />
+                    <div className="flex-layout space-between w-100">
+                        <div className="flex-layout">
+                            <input id='remember-me' type="checkbox"/> 
+                            <label for='remember-me' className="fs-13px">Remember me</label>
                         </div>
-                        <div className="mg-1 w-100">
-                        <button className="auth-btn">Login</button>
-                        <button className="auth-btn-secondary" onClick={(e)=>loginHandler(e,{email:'johndoe@gmail.com',password:'john123'})}>Login as Guest</button>
-                        </div>
-                        <Link className="link-txt" to="/signup">{"Create New Account >>"}</Link>
+                        <Link className="link-txt" to="*">Forgot Password ?</Link>      
                     </div>
-                </form>
-            </div>
-        </div>
+                    <div className="mg-1 w-100">
+                    <button className="auth-btn">Login</button>
+                    <button className="auth-btn-secondary" onClick={(e)=>loginHandler(e,{email:'johndoe@gmail.com',password:'john123'})}>Login as Guest</button>
+                    </div>
+                    <Link className="link-txt" to="/signup">{"Create New Account >>"}</Link>
+                </div>
+            </form>
         </div>
     )
 }
--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -11,34 +11,30 @@ function SignUp(){
     const { signupHandler } = useAuth()
 
     return(
-        <div>
-            <div className="flex-layout flex-center auth-parent">
-                <div>
-                <form className="flex-column glass-card signup-form" onSubmit={(e)=>signupHandler(e, user)}>
-                    <div className="flex-column flex-center">
-                        <Link to="/" className="flex-center">
-                            <img className="header-logo" src={logo}
-                                alt="playtube logo"/>
-                        <span className="header-title">PlayTube</span>
-                        </Link>
-                        <input type="text" placeholder="First Name" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, firstName: e.target.value})} />
-                        <input type="text" placeholder="Last Name" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, lastName: e.target.value})} />
-                        <input type="text" placeholder="Email" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, email: e.target.value})} />
-                        <input type="password" placeholder="Password" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, password: e.target.value})} />
-                        <div className="flex-layout w-100 space-between">
-                            <div className="flex-layout">
-                                <input id='terms' type="checkbox"/> 
-                                <label for='terms' className="fs-13px">I agree to all the terms and conditions.</label>
-                            </div>   
-                        </div>
-                        <div className="mg-1 w-100">
-                        <button className="auth-btn">SignUp</button>
-                        </div>
-                        <Link className="link-txt" to="/login">Login with existing account</Link>
+        <div className="flex-layout flex-center auth-parent">
+            <form className="flex-column glass-card signup-form" onSubmit={(e)=>signupHandler(e, user)}>
+                <div className="flex-column flex-center">
+                    <Link to="/" className="flex-center">
+                        <img className="header-logo" src={logo}
+                            alt="playtube logo"/>
+                    <span className="header-title">PlayTube</span>
+                    </Link>
+                    <input type="text" placeholder="First Name" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, firstName: e.target.value})} />
+                    <input type="text" placeholder="Last Name" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, lastName: e.target.value})} />
+                    <input type="text" placeholder="Email" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, email: e.target.value})} />
+                    <input type="password" placeholder="Password" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, password: e.target.value})} />
+                    <div className="flex-layout w-100 space-between">
+                        <div className="flex-layout">
+                            <input id='terms' type="checkbox"/> 
+                            <label for='terms' className="fs-13px">I agree to all the terms and conditions.</label>
+                        </div>   
                     </div>
-                </form>
-            </div>
-        </div>
+                    <div className="mg-1 w-100">
+                    <button className="auth-btn">SignUp</button>
+                    </div>
+                    <Link className="link-txt" to="/login">Login with existing account</Link>
+                </div>
+            </form>
         </div>
     )
 }
