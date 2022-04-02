a6e14c17a5db3fe9219c0a8094c777c93e606d72
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-04-02 21:25:04 +0530

    refactor : changes in styles and formatting of pages

--- src/components/menuPopUp/index.jsx
+++ src/components/menuPopUp/index.jsx
@@ -2,15 +2,26 @@ import {
     playlist,
     watchLater,
 } from "assets/icons";
+import { useWatchLater } from "context";
+import checkIfPresent from 'utils/functions/checkIfPresent'
 
-export function MenuPopUp(){
+export function MenuPopUp( {video} ){
+    const { watchLaterState, addToWatchLater, removeFromWatchLater } = useWatchLater()
     return (
         <ul className="menu-options flex-start">
-            <li className='menu-option'>
-                <div className="flex-layout">
-                <img className="menu-icon" src={watchLater} />Add to WatchLater
-                </div>
-            </li>
+            {
+                !checkIfPresent( video._id, watchLaterState) ?
+                <li className='menu-option' onClick={() => addToWatchLater(video)}>
+                    <div className="flex-layout">
+                    <img className="menu-icon" src={watchLater} />Add to Watch Later
+                    </div>
+                </li>
+                :<li className='menu-option' onClick={() => removeFromWatchLater(video)}>
+                    <div className="flex-layout">
+                    <img className="menu-icon" src={watchLater} />Remove from Watch Later
+                    </div>
+                </li>
+            }
             <li className='menu-option'>
                 <div className="flex-layout">
                 <img className="menu-icon" src={playlist} />Add to Playlist
