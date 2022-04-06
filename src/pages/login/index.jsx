698eb5501996462d61ebeb8ca7c4258c89bcfc3d
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-04-06 13:48:09 +0530

    refactor : show guest credentails in input boxes before logging in

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,7 +8,7 @@ import './login.css'
 function Login(){
 
     useDocumentTitle('Playtube | Login')
-    
+
     const [user, setUser] = useState({email:'',password:''})
     const { loginHandler } = useAuth()
 
@@ -21,8 +21,8 @@ function Login(){
                             alt="playtube logo"/>
                     <span className="header-title">PlayTube</span>
                     </Link>
-                    <input type="text" placeholder="Email" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, email:e.target.value})} />
-                    <input type="password" placeholder="Password" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, password:e.target.value})} />
+                    <input type="text" placeholder="Email" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, email:e.target.value})} value={user.email} />
+                    <input type="password" placeholder="Password" className='form-input mg-tb-1' onInput={(e)=>setUser({...user, password:e.target.value})} value={user.password}/>
                     <div className="flex-layout space-between w-100">
                         <div className="flex-layout">
                             <input id='remember-me' type="checkbox"/> 
@@ -32,7 +32,9 @@ function Login(){
                     </div>
                     <div className="mg-1 w-100">
                     <button className="auth-btn">Login</button>
-                    <button className="auth-btn-secondary" onClick={(e)=>loginHandler(e,{email:'johndoe@gmail.com',password:'john123'})}>Login as Guest</button>
+                    <button className="auth-btn-secondary" onClick={(e)=>{
+                        setUser({email:'johndoe@gmail.com',password:'john123'})
+                        loginHandler(e,{email:'johndoe@gmail.com',password:'john123'})}}>Login as Guest</button>
                     </div>
                     <Link className="link-txt" to="/signup">{"Create New Account >>"}</Link>
                 </div>
