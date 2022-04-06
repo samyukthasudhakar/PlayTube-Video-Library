6f819dfea8286ab048233a400a92ae764676a834
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-04-06 23:18:34 +0530

    refactor : added proper indent to code

--- src/pages/playlist/index.jsx
+++ src/pages/playlist/index.jsx
@@ -30,7 +30,7 @@ function PlaylistPage(){
                         <div className="flex-layout" >
                             {
                                 playlists.map((item,key)=>
-                                <div className="flex-column flex-center glass-card playlist-card" >
+                                <div className="flex-column flex-center playlist-card" >
                                 <Link to={`/playlist/:${item._id}`}><h2 key={key}>{item.title}</h2></Link>
                                 <p className='p-sm'>{`${item.videos.length} ${item.videos.length===1 ? 'video':'videos'}`}</p>
                                 <div className="flex-layout">
--- src/pages/singleVideoPage/index.jsx
+++ src/pages/singleVideoPage/index.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import { Header, SideNavBar, PlayListPopup } from 'components'
+import { Header, SideNavBar, PlayListPopup, VideoRecommendationCard } from 'components'
 import { addToPlaylist, watchLater } from 'assets/icons'
-import { ReactComponent as Like } from 'assets/icons/like.svg';
-import { ReactComponent as Dislike } from 'assets/icons/dislike.svg';
-import { useAuth, useVideos, useLikedVideos, useHistory } from 'context';
+import { ReactComponent as Like } from 'assets/icons/like.svg'
+import { useAuth, useVideos, useLikedVideos, useHistory, useWatchLater } from 'context'
 import { useDocumentTitle, useScrollToTop } from 'utils/hooks'
+import checkIfPresent from 'utils/functions/checkIfPresent'
 
 import './singleVideoPage.css'
 import { useEffect } from 'react';
@@ -13,22 +13,25 @@ import { useEffect } from 'react';
 function SingleVideoPage(){
     
     useDocumentTitle('Playtube | Watch')
-
+    useScrollToTop()
+    
     const { data: {videos} } = useVideos()
     let { videoID } = useParams()
     videoID = videoID.split(':')[1]
     const [video]  = videos.filter(video => video._id == videoID)
-    const {_id, src, title, views, date} = video
+    const {_id, src, title, views, date, icon, creator, category} = video
     const { likedVideos, likeToggle } = useLikedVideos()
     const liked = likedVideos.filter(video => video._id == _id).length > 0 ? "black" : "none"
     const { addToHistory } = useHistory()
     const {authState:{isLoggedIn}} = useAuth()
     const [playlistToggle, setPlaylistToggle] = useState(false)
+    const { watchLaterState, addToWatchLater, removeFromWatchLater } = useWatchLater()
     const navigateTo = useNavigate()
-    useScrollToTop()
+    
     useEffect(()=>{
         addToHistory(video)
     },[])
+
     return (
         <div>
             <Header />
@@ -36,26 +39,34 @@ function SingleVideoPage(){
             <SideNavBar />
             <div className='flex-layout video-listing padding-2'>
             <div>
-            <iframe width="730" height="400" style={{border:'none',margin:'0 2rem', borderRadius:'3px'}}
+            <iframe width="700" height="400" style={{border:'none',margin:'0 2rem', borderRadius:'3px'}}
             src={`https://www.youtube.com/embed/${src}`}>
             </iframe>
             <p className="single-video-title">{`${title}`}</p>
-            <div className='flex-layout'>
+            <div className='flex-layout flex-end'>
                 <p className="video-details-secondary">{`${views} views • ${date}`}</p>
                 <p className="flex-layout flex-center padding-rl-2" onClick={()=>likeToggle(video)}><Like fill={liked}/>244k</p>
-                <p className="flex-layout flex-center padding-rl-2" >{<Dislike fill="none" />}Dislike</p>
                 <div className="flex-layout flex-center padding-rl-2" onClick={()=>setPlaylistToggle(!playlistToggle)}><img src={addToPlaylist} style={{width: "2rem", height: '2rem', marginRight:"8px"}}/>Save</div>
-                <p className="flex-layout flex-center padding-rl-2" ><img src={watchLater} style={{width: "1.2rem", height: '1.2rem', marginRight:"8px"}}/>Watch Later</p>
+                <p className="flex-layout flex-center padding-rl-2" onClick={() =>{
+                    !checkIfPresent( video._id, watchLaterState) ?
+                    addToWatchLater(video):
+                    removeFromWatchLater(video)
+                }}><img src={watchLater} style={{width: "1.2rem", height: '1.2rem', marginRight:"8px"}}/>Watch Later</p>
+            </div>
+            <div style={{display:"flex", justifyContent:"flex-start", alignItems:"flex-start"}}>
+            <div className="flex-layout flex-center">
+            <img className="avatar mg-8px" src={icon}/>
+            <p class="p-md p-bold">{creator}</p>
             </div>
             </div>
-            {/* please ignore : this is temporary code until i build the sidebar with suggested videos listed */}
+            </div>
             <div className='flex-column'>
-            <div style={{width:'20rem', height: '10rem', backgroundColor: "grey", color: "grey", marginBottom: "1.5rem", marginRight:"1rem"}}>.</div>
-            <div style={{width:'20rem', height: '10rem', backgroundColor: "grey", color: "grey", marginBottom: "1.5rem", marginRight:"1rem"}}>.</div>
-            <div style={{width:'20rem', height: '10rem', backgroundColor: "grey", color: "grey", marginBottom: "1.5rem", marginRight:"1rem"}}>.</div>
-            <div style={{width:'20rem', height: '10rem', backgroundColor: "grey", color: "grey", marginBottom: "1.5rem", marginRight:"1rem"}}>.</div>
-            <div style={{width:'20rem', height: '10rem', backgroundColor: "grey", color: "grey", marginBottom: "1.5rem", marginRight:"1rem"}}>.</div>
-            <div style={{width:'20rem', height: '10rem', backgroundColor: "grey", color: "grey", marginBottom: "1.5rem", marginRight:"1rem"}}>.</div>
+            <div className="flex-layout flex-start" style={{marginBottom:"1rem"}}>
+            <p className="active-category-link" style={{width:"content-fit"}}>{category}</p>
+            </div>
+            {
+                videos.filter(video => video._id != videoID && video.category == category ).map(video => <VideoRecommendationCard video={video} /> )
+            }
             </div>
             </div>
             </div>
